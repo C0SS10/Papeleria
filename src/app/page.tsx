03a9075e5e0cc7fd6ec509f8cfa320d4f287029c
C0SS10 <estebancg12@hotmail.com>
2024-04-23 09:16:36 -0500

    Update Home page layout and add banner image 🎨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import HomeBanner from "@/app/components/HomeBanner";
+import { Section } from "@/app/components/Section";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>hola</h1>
+    <div className="p-4">
+      <Section>
+        <div>
+          <HomeBanner />
+        </div>
+      </Section>
     </div>
   );
 }
