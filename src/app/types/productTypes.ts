a6b8fffce52b6684b00301ca845e1fe801a5b6d9
C0SS10 <estebancg12@hotmail.com>
2024-04-28 19:38:18 -0500

    Update productTest description and add maxStock property 🧰

--- src/app/types/productTypes.ts
+++ src/app/types/productTypes.ts
@@ -2,6 +2,10 @@ interface ProductData {
   id: number;
   title: string;
   price: number;
+  stock: number;
+  maxStock: number;
+  discountPercentage: number;
+  category: string;
   thumbnail: string;
   description: string;
   images: string[];
@@ -10,3 +14,16 @@ interface ProductData {
 export interface ProductCardProps {
   product: ProductData
 }
+
+export type CartProductType ={
+  id: number;
+  title: string;
+  price: number;
+  stock: number;
+  maxStock: number;
+  discountPercentage: number;
+  category: string;
+  description: string;
+  thumbnail: string;
+  images: string[];
+};
--- src/app/utils/productTest.ts
+++ src/app/utils/productTest.ts
@@ -1,10 +1,11 @@
 export const productTest = {
   id: 1,
   title: "Teclado + Mouse + Auriculares",
-  description: "Combo de perifericos gaming",
+  description: "Combo de perifericos gaming con luces RGB y conexión USB 2.0. Incluye teclado, mouse y auriculares con micrófono.",
   price: 299900,
   discountPercentage: 44,
   stock: 5,
+  maxStock: 8,
   category: "electronicos",
   thumbnail:
     "https://panamericana.vtexassets.com/arquivos/ids/514395-800-auto",
