3b8c9b48c3226050e0862143051a8e49ad567aaa
C0SS10 <estebancg12@hotmail.com>
2024-04-29 09:49:44 -0500

    Add HabeasData and PoliticaDevolucion page components 📄

---
+++ src/app/habeas-data/page.tsx
@@ -0,0 +1,7 @@
+export default function HabeasData() {
+  return(
+    <div>
+      <h1>HabeasData</h1>
+    </div>
+  )
+}
---
+++ src/app/politicas-de-devolucion/page.tsx
@@ -0,0 +1,7 @@
+export default function PoliticaDevolucion() {
+  return(
+    <div>s
+      <h1>PoliticaDevolución</h1>
+    </div>
+  )
+}
