ad63db4e605272b81dea28b0304e4e26d2916e3d
C0SS10 <estebancg12@hotmail.com>
2024-05-05 18:24:38 -0500

    Update registration route and form components 🧰

--- src/app/registro/RegisterForm.tsx
+++ src/app/registro/RegisterForm.tsx
@@ -6,6 +6,10 @@ import Input from "@/app/components/inputs/Input";
 import { RiAccountPinBoxFill } from "react-icons/ri";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -21,10 +25,34 @@ const RegisterForm = () => {
     },
   });
 
+  const router = useRouter();
+
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     setIsLoading(true);
-    console.log(data);
-    setIsLoading(false);
+    axios
+      .post("/api/registro", data)
+      .then((response) => {
+        toast.success("¡Cuenta creada!");
+        signIn("credentials", {
+          email: data.email,
+          password: data.password,
+          redirect: false,
+        }).then((callback) => {
+          if (callback?.ok) {
+            router.push("/carrito");
+            router.refresh();
+            toast.success("¡Bienvenido!");
+          }
+
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        });
+      })
+      .catch(() => {
+        toast.error("Error al crear la cuenta");
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
@@ -66,11 +94,15 @@ const RegisterForm = () => {
         className="bg-transparent font-bold p-2 shadow-sm rounded-sm text-center text-xl w-full text-pistachio-400 hover:bg-pistachio-400 hover:text-slate-200 border-2 border-pistachio-400 transition-colors ease-in flex flex-row items-center justify-center gap-2"
         onClick={handleSubmit(onSubmit)}
       >
-        <FcGoogle />Registrate usando Google
+        <FcGoogle />
+        Registrate usando Google
       </button>
       <p className="flex flex-row items-center justify-center gap-2 text-lg font-bold">
-        <RiAccountPinBoxFill className="text-pistachio-900"/> ¿Ya tienes cuenta?
-        <Link href="/ingreso" className="hover:underline">Ingresa</Link>
+        <RiAccountPinBoxFill className="text-pistachio-900" /> ¿Ya tienes
+        cuenta?
+        <Link href="/ingreso" className="hover:underline">
+          Ingresa
+        </Link>
       </p>
     </>
   );
